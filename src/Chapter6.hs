b62763696b68d3d49abfb9a4467b43f2220ca400
David Burkett <db.discodave@gmail.com>
2019-12-21 11:23:43 -0500

    ajdslkdsajlk

---
+++ src/Chapter6.hs
@@ -0,0 +1,29 @@
+module Chapter6 where
+
+import           Control.Lens
+
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
+
+data Role
+  = Gunner
+  | PowderMonkey
+  | Navigator
+  | Captain
+  | FirstMate
+  deriving (Show, Eq, Ord)
+
+data CrewMember = CrewMember
+  { _name :: String
+  , _role :: Role
+  , _talents :: [String]
+  } deriving (Show, Ord, Eq)
+makeLenses ''CrewMember
+
+roster :: Set CrewMember
+roster = Set.fromList
+  [ CrewMember "Grumpy Roger"     Gunner       ["Juggling", "Arbitrage"]
+  , CrewMember "Long-John Bronze" PowderMonkey ["Origami"]
+  , CrewMember "Salty Steve"      PowderMonkey ["Charcuterie"]
+  , CrewMember "One-eyed Jack"    Navigator    []
+  ]
